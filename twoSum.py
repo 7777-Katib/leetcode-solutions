48329d1beff86b97f2b3c358887e469c1f8e2966
7777-Katib <85574228+7777-Katib@users.noreply.github.com>
2022-01-22 12:47:37 +0600

    dhsfh

--- twoSum.py
+++ twoSum.py
@@ -1,1 +1,16 @@
-print("hello world")
+
+class Solution:
+    def twoSum(self, nums, target):
+        self.nums = nums
+        self.target = target
+        my = [0]*2
+        l = len(nums)
+        for i in range(l):
+            for j in range(i+1, l):
+                if nums[i]+nums[j] == target:
+                    return [i, j]
+
+
+nums = [7, 2, 11, 15]
+now = Solution()
+now.twoSum(nums, 9)
